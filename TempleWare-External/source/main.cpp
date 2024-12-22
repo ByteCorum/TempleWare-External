5669db71e8ef35435286d45de2f21d4ceae7e411
TempleTerry[x64] <123938029+PhilipPanda@users.noreply.github.com>
2024-12-22 14:00:26 +0100

    Revert "Merge pull request #11 from ByteCorum/main"
    This reverts commit 6f254637e839b0b6c2b1054da9abf7e3fb839b92, reversing
    changes made to bee84a8f02962a8698dac19c6f81eca9fccc1009.

--- TempleWare-External/source/main.cpp
+++ TempleWare-External/source/main.cpp
@@ -8,53 +8,50 @@
 #include "globals/globals.h"
 #include "threads/threads.h"
 #include "offsets/offsets.h"
+
 #include <thread>
 
-int __stdcall wWinMain(HINSTANCE instance, HINSTANCE previousInstance, PWSTR arguments, int commandShow) 
-{
+int __stdcall wWinMain(
+    HINSTANCE instance,
+    HINSTANCE previousInstance,
+    PWSTR arguments,
+    int commandShow) {
+
     if (!offsets::UpdateOffset())
         return EXIT_FAILURE;
 
-    Memory memory("cs2.exe");
+    const auto memory = Memory("cs2.exe");
+
     globals::client = memory.GetModuleAddress("client.dll");
 
-    std::vector<std::thread> threads;
-    threads.emplace_back(threads::RunMiscThread, std::ref(memory));
-    threads.emplace_back(threads::RunVisualThread, std::ref(memory));
-    threads.emplace_back(threads::RunAimThread, std::ref(memory));
-
-    for (auto& thread : threads)
-        thread.detach();
+    std::thread(threads::RunMiscThread, std::ref(memory)).detach();
+    std::thread(threads::RunVisualThread, std::ref(memory)).detach();
+    std::thread(threads::RunAimThread, std::ref(memory)).detach();
 
     gui::CreateHWindow("templecheats.xyz");
     gui::CreateDevice();
-
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NoMouse;
+    gui::CreateImGui();
 
     bool windowVisible = true;
-    while (globals::isRunning)
-    {
-        if (GetAsyncKeyState(VK_END) & 0x8000) 
-        {
+
+    while (globals::isRunning) {
+        if (GetAsyncKeyState(VK_END) & 0x8000) {
             windowVisible = !windowVisible;
             ShowWindow(gui::window, windowVisible ? SW_SHOW : SW_HIDE);
             std::this_thread::sleep_for(std::chrono::milliseconds(200));
         }
 
-        if (windowVisible) 
-        {
+        if (windowVisible) {
             gui::BeginRender();
             gui::Render();
-            ImGui::Render();
             gui::EndRender();
         }
-        else
+        else {
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        }
     }
 
-    ImGui::DestroyContext();
+    gui::DestroyImGui();
     gui::DestroyDevice();
     gui::DestroyHWindow();
 
