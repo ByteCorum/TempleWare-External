6672fe2107532b4361f7b7d3e490c3abf064d8f0
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2025-01-23 16:56:44 +0200

    situation with fov now much better

--- TempleWare-External/source/features/fov.cpp
+++ TempleWare-External/source/features/fov.cpp
@@ -1,11 +1,19 @@
 #include "fov.h"
 #include "../globals/globals.h"
 #include "../offsets/offsets.h"
+#include <thread>
 
 namespace features 
 {
     void FOVManager::AdjustFOV(const Memory& memory) noexcept
     {
+        if (globals::lastFOV != globals::FOV)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+
+        globals::lastFOV = globals::FOV;
+
         std::uintptr_t localPlayer = memory.Read<std::uintptr_t>(globals::client + offsets::dwLocalPlayerPawn);
         if (!localPlayer)
             return;
@@ -16,7 +24,7 @@ namespace features
 
         std::uint16_t currentFov = memory.Read<std::uint16_t>(cameraServices + offsets::m_iFOV);
         bool isScoped = memory.Read<bool>(localPlayer + offsets::m_bIsScoped);
-        std::uint16_t desiredFov = static_cast<uint16_t>(globals::FOV);
+        std::uint16_t desiredFov = static_cast<uint16_t>(globals::lastFOV);
 
         if (!isScoped && currentFov != desiredFov)
             memory.Write<uint16_t>(cameraServices + offsets::m_iFOV, desiredFov);
